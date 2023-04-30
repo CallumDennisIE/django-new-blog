a38fb616601fbee9aa11e461ec94f4e4f9f243ab
CallumDennisIE <callumdennisie@gmail.com>
2023-04-30 21:27:22 +0000

    feat: add authorisation

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 ALLOWED_HOSTS = ['8000-callumdennisie-django-ne-x9zwdeyp2d.us2.codeanyapp.com',
                  'django-blog-new.herokuapp.com', 'localhost']
 
@@ -41,12 +43,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGout_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
